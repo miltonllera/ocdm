24e346b734517f4325bc2f6c3da34c81fc7c2286
Milton Montero <lleramontero@gmail.com>
2023-11-23 11:09:51 +0100

    Test figure-ground segmentation on non-pentominos.

--- scripts/score_models.sh
+++ scripts/score_models.sh
@@ -88,17 +88,33 @@
 #python -m bin.analyze analysis=score_slot_recons run_path=$SA_PENTOMINO_6SHAPE
 
 
-#------------------------ Pretrained model on non-pentominos ------------------------------------
+#------------------------ Pretrained SA model on non-pentominos ------------------------------------
 
-PENTOMINO_PRETRAINED=data/logs/unsupervised/pentominos/slot_ae/new_shape/2023-04-23_21-21
+# PENTOMINO_PRETRAINED=data/logs/unsupervised/pentominos/slot_ae/new_shape/2023-04-23_21-21
 
 # python -m bin.analyze analysis=score_slot_recons \
 #   run_path=$PENTOMINO_PRETRAINED \
 #   +dataset@overrides.dataset=non_pentominos \
 #   +overrides.dataset.split_sizes="[0.01, 0.01, 0.98]"
 
+# python -m bin.analyze analysis=slot_recons \
+#   run_path=$PENTOMINO_PRETRAINED \
+#   +dataset@overrides.dataset=non_pentominos \
+#   +overrides.dataset.split_sizes="[0.00, 0.00, 1.00]" \
+#   visualizations.slot_reconstruction.data_split=test
+
+
+#------------------------ Pretrained FG-Seg model on non-pentominos --------------------------------
+
+PENTOMINO_PRETRAINED=data/logs/unsupervised/pentominos/fgseg/baseline/2023-11-23_10-17
+
 python -m bin.analyze analysis=slot_recons \
   run_path=$PENTOMINO_PRETRAINED \
   +dataset@overrides.dataset=non_pentominos \
   +overrides.dataset.split_sizes="[0.00, 0.00, 1.00]" \
   visualizations.slot_reconstruction.data_split=test
+
+python -m bin.analyze analysis=score_slot_recons \
+  run_path=$PENTOMINO_PRETRAINED \
+  +dataset@overrides.dataset=non_pentominos \
+  +overrides.dataset.split_sizes="[0.01, 0.01, 0.98]"
