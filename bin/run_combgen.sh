7b9791427cb5e82b77f1bfb047ffbae3d6c3e6c6
Milton Montero <lleramontero@gmail.com>
2026-03-31 14:04:14 +0200

    Re-run experiments.

--- bin/run_combgen.sh
+++ bin/run_combgen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=1
 
 #------------------------------- Replication from LiLS -----------------------------------
 
-# for i in {1..1}; do
+# for i in {1..3}; do
   # uv run python -m scripts.train experiment=combgen/dsprites_heart_position_vae model.latent_size=8
   # uv run python -m scripts.train experiment=combgen/dsprites_heart_position_wae model.latent_size=8
   # uv run python -m scripts.train experiment=combgen/dsprites_heart_position_sa model.slot_size=8
@@ -43,7 +43,7 @@ export CUDA_VISIBLE_DEVICES=2
 
 #------------------------------- Replicating failures ------------------------------------
 
-# for i in {1..1}; do
+# for i in {1..3}; do
 #   uv run python -m scripts.train experiment=combgen/dsprites_heart_rotation_vae model.latent_size=8
 #   uv run python -m scripts.train experiment=combgen/dsprites_heart_rotation_wae model.latent_size=8
 #   uv run python -m scripts.train experiment=combgen/dsprites_heart_rotation_sa model.slot_size=8
@@ -76,9 +76,9 @@ for i in {1..1}; do
 #   uv run python -m scripts.train experiment=combgen/pentominos_rotation_wae model.latent_size=32
 #   uv run python -m scripts.train experiment=combgen/pentominos_rotation_sa model.slot_size=32
 
-  uv run python -m scripts.train experiment=combgen/pentominos_rotation_vae model.latent_size=64
-#   uv run python -m scripts.train experiment=combgen/pentominos_rotation_wae model.latent_size=64
-#   uv run python -m scripts.train experiment=combgen/pentominos_rotation_sa model.slot_size=64
+  # uv run python -m scripts.train experiment=combgen/pentominos_rotation_vae model.latent_size=64
+  # uv run python -m scripts.train experiment=combgen/pentominos_rotation_wae model.latent_size=64
+  uv run python -m scripts.train experiment=combgen/pentominos_rotation_sa model.slot_size=64
 done
 
 
